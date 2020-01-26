ddb67b1d4a0eda0c57d34225e39440ee7ceb5968
LFMP <luizfellipe42@hotmail.com>
2020-01-26 13:45:02 -0300

    locate axis

--- lib/utils/magicalpaint.dart
+++ lib/utils/magicalpaint.dart
@@ -8,6 +8,77 @@ class MagicalPaint extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
+    // Paint para alocar eixos
+    Paint axisPaint = new Paint()
+      ..strokeWidth = 2.0
+      ..strokeCap = StrokeCap.round
+      ..color = Colors.grey
+      ..style = PaintingStyle.fill;
+
+    canvas.drawLine(Offset(0, 2), Offset(50, 2), axisPaint);
+    canvas.drawLine(Offset(2, 0), Offset(2, 50), axisPaint);
+    Path xPath = Path();
+    xPath.moveTo(50, 2);
+    xPath.lineTo(50, 6);
+    xPath.lineTo(56, 2);
+    xPath.lineTo(50, 0);
+    xPath.close();
+    canvas.drawPath(xPath, axisPaint);
+
+    Path yPath = Path();
+    yPath.moveTo(2, 50);
+    yPath.lineTo(6, 50);
+    yPath.lineTo(2, 56);
+    yPath.lineTo(0, 50);
+    yPath.close();
+    canvas.drawPath(yPath, axisPaint);
+
+    TextSpan eixoX = TextSpan(
+      style: TextStyle(
+        color: Colors.grey,
+        fontSize: 10,
+      ),
+      text: 'X',
+    );
+    TextPainter tp = TextPainter(
+      text: eixoX,
+      textAlign: TextAlign.left,
+      textDirection: TextDirection.ltr,
+    );
+    tp.layout();
+    tp.paint(canvas, Offset(42, 4));
+
+    TextSpan eixoY = TextSpan(
+      style: TextStyle(
+        color: Colors.grey,
+        fontSize: 10,
+      ),
+      text: 'Y',
+    );
+    TextPainter tp2 = TextPainter(
+      text: eixoY,
+      textAlign: TextAlign.left,
+      textDirection: TextDirection.ltr,
+    );
+    tp2.layout();
+    tp2.paint(canvas, Offset(4, 42));
+
+    TextSpan zerinho = TextSpan(
+      style: TextStyle(
+        color: Colors.grey,
+        fontSize: 10,
+      ),
+      text: '0',
+    );
+    TextPainter origem = TextPainter(
+      text: zerinho,
+      textAlign: TextAlign.left,
+      textDirection: TextDirection.ltr,
+    );
+    origem.layout();
+    origem.paint(canvas, Offset(4, 4));
+
+    // Paint para desenhar figuras
     Paint paint = new Paint()
       ..strokeWidth = 5.0
       ..strokeCap = StrokeCap.round
