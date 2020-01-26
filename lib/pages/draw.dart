9db2f7b4bf6bd1b8a45d12cd0ce8188d43d5fad0
LFMP <luizfellipe42@hotmail.com>
2020-01-25 23:33:25 -0300

    rotacao pro circulo implementada

--- lib/pages/draw.dart
+++ lib/pages/draw.dart
@@ -169,6 +169,37 @@ class _DrawPageState extends State<DrawPage> {
                   resultLine.getColumn(1)[0],
                   resultLine.getColumn(1)[1],
                 ),
+              },
+              if (f.forma == Forma.circulo)
+              {
+                resultLine = math.Matrix3.columns(
+                  math.Vector3(cosseno, seno, 0),
+                  math.Vector3(-seno, cosseno, 0),
+                  math.Vector3(
+                    (f.pontos[0].dy * seno) -
+                        (f.pontos[0].dx * cosseno) +
+                        f.pontos[0].dx,
+                    -(f.pontos[0].dx * seno) -
+                        (f.pontos[0].dy * cosseno) +
+                        f.pontos[0].dy,
+                    1,
+                  ),
+                ),
+                resultLine.multiply(
+                  math.Matrix3.columns(
+                    math.Vector3(f.pontos[0].dx, f.pontos[0].dy, 1),
+                    math.Vector3(f.pontos[1].dx, f.pontos[1].dy, 1),
+                    math.Vector3(0, 0, 0),
+                  ),
+                ),
+                f.pontos[0] = Offset(
+                  resultLine.getColumn(0)[0],
+                  resultLine.getColumn(0)[1],
+                ),
+                f.pontos[1] = Offset(
+                  resultLine.getColumn(1)[0],
+                  resultLine.getColumn(1)[1],
+                ),
               }
           },
         );
