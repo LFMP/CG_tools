8958145189125aac01df93b7dd1f8382cea31239
LFMP <luizfellipe42@hotmail.com>
2020-01-29 00:14:31 -0300

    add esqueleto do ajuda

--- lib/pages/help.dart
+++ lib/pages/help.dart
@@ -1,3 +1,4 @@
+import 'package:cg_tools/utils/appstyle.dart';
 import 'package:flutter/material.dart';
 
 Widget ajudaContexto() {
@@ -6,15 +7,150 @@ Widget ajudaContexto() {
       borderRadius: BorderRadius.vertical(),
     ),
     child: ListView(
+      padding: EdgeInsets.only(left: 8.0, right: 8.0),
       children: <Widget>[
         ExpansionTile(
           title: Text('Desenhar formas'),
+          children: <Widget>[
+            Text('Linha'),
+            Divider(
+              thickness: 2.0,
+              color: AppStyle.primary,
+            ),
+            Text('aaaaaaaaaa'),
+            Divider(
+              thickness: 2.0,
+              color: AppStyle.primary,
+            ),
+            Text('Triangulo'),
+            Divider(
+              thickness: 2.0,
+              color: AppStyle.primary,
+            ),
+            Text('aaaaaaaaaa'),
+            Divider(
+              thickness: 2.0,
+              color: AppStyle.primary,
+            ),
+            Text('Quadrado'),
+            Divider(
+              thickness: 2.0,
+              color: AppStyle.primary,
+            ),
+            Text('aaaaaaaaaa'),
+            Divider(
+              thickness: 2.0,
+              color: AppStyle.primary,
+            ),
+            Text('Circulo'),
+            Divider(
+              thickness: 2.0,
+              color: AppStyle.primary,
+            ),
+            Text('aaaaaaaaaa'),
+          ],
         ),
         ExpansionTile(
           title: Text('Selecionar objetos'),
+          children: <Widget>[
+            Text('Selecionar todos'),
+            Divider(
+              thickness: 2.0,
+              color: AppStyle.primary,
+            ),
+            Text('aaaaaaaaaa'),
+            Divider(
+              thickness: 2.0,
+              color: AppStyle.primary,
+            ),
+            Text('Selecao individual'),
+            Divider(
+              thickness: 2.0,
+              color: AppStyle.primary,
+            ),
+            Text('aaaaaaaaaa'),
+          ],
         ),
         ExpansionTile(
           title: Text('Operacoes'),
+          children: <Widget>[
+            Text('Rotacao 90 graus'),
+            Divider(
+              thickness: 2.0,
+              color: AppStyle.primary,
+            ),
+            Text('aaaaaaaaaa'),
+            Divider(
+              thickness: 2.0,
+              color: AppStyle.primary,
+            ),
+            Text('Rotacao'),
+            Divider(
+              thickness: 2.0,
+              color: AppStyle.primary,
+            ),
+            Text('aaaaaaaaaa'),
+            Divider(
+              thickness: 2.0,
+              color: AppStyle.primary,
+            ),
+            Text('Translacao'),
+            Divider(
+              thickness: 2.0,
+              color: AppStyle.primary,
+            ),
+            Text('aaaaaaaaaa'),
+            Divider(
+              thickness: 2.0,
+              color: AppStyle.primary,
+            ),
+            Text('Mudanca de escala'),
+            Divider(
+              thickness: 2.0,
+              color: AppStyle.primary,
+            ),
+            Text('aaaaaaaaaa'),
+            Divider(
+              thickness: 2.0,
+              color: AppStyle.primary,
+            ),
+            Text('Limpar tela'),
+            Divider(
+              thickness: 2.0,
+              color: AppStyle.primary,
+            ),
+            Text('aaaaaaaaaa'),
+            Divider(
+              thickness: 2.0,
+              color: AppStyle.primary,
+            ),
+            Text('Desfazer'),
+            Divider(
+              thickness: 2.0,
+              color: AppStyle.primary,
+            ),
+            Text('aaaaaaaaaa'),
+            Divider(
+              thickness: 2.0,
+              color: AppStyle.primary,
+            ),
+            Text('Refazer'),
+            Divider(
+              thickness: 2.0,
+              color: AppStyle.primary,
+            ),
+            Text('aaaaaaaaaa'),
+            Divider(
+              thickness: 2.0,
+              color: AppStyle.primary,
+            ),
+            Text('Excluir selecionados'),
+            Divider(
+              thickness: 2.0,
+              color: AppStyle.primary,
+            ),
+            Text('aaaaaaaaaa'),
+          ],
         ),
         ExpansionTile(
           title: Text('Linha de comando'),
