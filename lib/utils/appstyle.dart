f6a0b475b6bc4f0f94625335f538c6a53c4dd4d3
LFMP <luizfellipe42@hotmail.com>
2020-01-31 15:27:36 -0300

    color change

--- lib/utils/appstyle.dart
+++ lib/utils/appstyle.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/widgets.dart';
 
 class AppStyle {
-  static const Color primary = Color(0xddd50000);
+  static const Color primary = Color.fromRGBO(92, 21, 88, 1);
   static const Color complementary = Color(0x9990ee02);
   static const Color analagous1 = Color(0x00021aee);
   static const Color analagous2 = Color(0xddd602ee);
-  static const Color triadic1 = Color(0xffff5131);
+  static const Color triadic1 = Color.fromRGBO(175, 40, 167, 1);
   static const Color triadic2 = Color(0xeeee6002);
   static const Color white = Color(0xffffffff);
   static const Color black = Color(0x00000000);
